15aa0b9e661253a7a1e8b53684c0f12ff07d2d3c
Manju KC <kcmanjuthapa@gmail.com>
2020-02-11 19:33:37 -0500

    finished mvp

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -6,35 +6,51 @@ class DynamicArray:
 
     def insert(self, index, value):
         if self.count == self.capacity:
-            # TODO: increase size
-            print("ERROR: Array is full")
-            return
-
-        if index >= self.count:
-            # TODO: better error handling
-            print("Error: Index out of bounds")
-            return
+             '''
+        Insert object before index.
+        '''
+        # Check if there's room in our storage
+        if self.count >= self.capacity:
+            # If not, return an error
+            self.double_size()
 
         for i in range(self.count, index, -1):
             self.storage[i] = self.storage[i - 1]
-
+       #Insert our value
         self.storage[index] = value
+        #Increment count
         self.count += 1
 
     def append(self, value):
         if self.count == self.capacity:
-            # TODO: increase size
-            print("ERROR: Array is full")
-            return
+            self.double_size()
 
         self.count += 1
         self.storage[self.count - 1] = value
 
     def double_size(self):
+         # Double storage size
         self.capacity *= 2
         new_storage = [None] * self.capacity
-
+         # Copy old items to new storage
         for i in range(self.count):
             new_storage[i] = self.storage[i]
-
+        # Point storage to the new storage
         self.storage = new_storage
+
+        arr = DynamicArray(8)
+        arr.insert(0, "z")
+        arr.insert(0, "y")
+        arr.insert(0, "x")
+        arr.insert(0, "w")
+        print(arr.storage)
+        arr.append("a")
+        arr.append("b")
+        arr.append("c")
+        arr.append("d")
+        print(arr.storage)
+        arr.append(1)
+        arr.append(2)
+        arr.append(3)
+        arr.append(4)
+        print(arr.storage)
--- src/hashtable.py
+++ src/hashtable.py
@@ -51,7 +51,27 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+
+        # here we find the index of the node which by using '_hash_mod' we create an 
+        # integer of the key which will be the index on the list where we want to insert it
+        index = self._hash_mod(key)
+        # then we retrieve any preexisting node location before inserting it which should be None or occupied
+        current = self.storage[index]
+           
+        # Try to find node already allocated with the same key
+        while (current is not None and current.key is not key): #collision
+            last = current
+            current = last.next
+
+        # If a node is found with the same key
+        if (current is not None):
+            current.value = value
+        # if a node is not found
+        else:
+            # then we create a node pair using "LinkedPair" using the key and value
+            pair = LinkedPair(key, value)
+            pair.next = self.storage[index]
+            self.storage[index] = pair
 
 
 
@@ -63,7 +83,29 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        # here we find the index of the node which by using '_hash_mod' we create an 
+        # integer of the key which will be the index on the list where we want to insert it
+        index = self._hash_mod(key)
+        # then we retrieve any preexisting node location before inserting it which should be None or occupied
+        last = None
+        current = self.storage[index]
+
+        # While the node isn't located, keep traversing
+        while (current is not None and current.key is not  key):
+            last = current
+            current = current.next
+        # If the key couldn't be found
+        if (self.storage[index] is None):
+            print("Error! Couldn't find key.")
+        # if it is found
+        else:
+            # Removing the first element in the linkedlist
+            if (last is None):
+                self.storage[index] = current.next
+            else:
+                last.next = current.next
+    
+    
 
 
     def retrieve(self, key):
@@ -75,6 +117,21 @@ class HashTable:
         Fill this in.
         '''
         pass
+      
+        # so we find the node here we start with it's index
+        index = self._hash_mod(key)
+        # then we find the node
+        node = self.storage[index]
+        # if the node is not none meaning it exists
+        while node is not None:
+            # But we have to make sure the key matches the key of the retrieved peraminter
+            if node.key == key:
+                # if it doesn't then return it
+                return node.value
+                # if not then we just continue on the list
+                # if it doesn't exist then we get nothing don't feel like returning anything 
+            node = node.next
+       
 
 
     def resize(self):
@@ -84,11 +141,26 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+     
+        self.capacity *= 2  # double the capacity
+        old_storage = self.storage
+        # create new hashtable/array with the increased capacity
+        self.storage = [None] * self.capacity
+        # for each bucket in new_storage array
+        for each_bucket in old_storage: 
+         # create iterator for each bucket 
+            curr_bucket = each_bucket 
+            # while we still have elements in our buckets
+            while curr_bucket:  
+                self.insert(curr_bucket.key, curr_bucket.value)
+                curr_bucket = curr_bucket.next
+   
+    
 
 
 
 if __name__ == "__main__":
+
     ht = HashTable(2)
 
     ht.insert("line_1", "Tiny hash table")
